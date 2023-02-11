1eae9b602503d9b0ca2cf1696aaf95c387a350cd
Débora  Fernandes Costa <116604446+deborabckp@users.noreply.github.com>
2023-02-11 14:16:01 -0300

    Create soma.cpp

---
+++ c++/soma.cpp
@@ -0,0 +1,20 @@
+#include <iostream> //Biblioteca principal
+using namespace std; 
+
+int main(){
+
+    int a,b; //Declaração de duas variáveis do tipo inteiro
+
+    cout << "Digite um numero:\t"; //Imprimindo na tela a mensagem
+
+    cin >> a; //Pedindo um número ao usuário 
+
+    cout << "Digite outro numero:\t"; //Imprimindo na tela a mensagem
+
+    cin >> b; //Pedindo outro número ao usuário
+
+    cout << "O resultado é: \t" << a+b << endl; //Mostrando na tela o resultado da conta
+
+    return 0;
+
+}
